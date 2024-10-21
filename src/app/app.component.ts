d0fe70c9266c8ff52b5b49fe63189e7a5a8cac7a
ARONSEIXAS\aronseixas <aronseixas@yahoo.com.br>
2024-10-20 21:59:51 -0300

    Preparação Video 68. Comentando tudo para ter um projeto limpo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  name: string = 'Aron';
-  minhaProp: boolean = false;
-  personSelectedIndex: number | undefined;
   
-  listPeople = [
-    { name: 'Aron',   idade: 20},
-    { name: 'Aron 2', idade: 21},
-    { name: 'Aron3',  idade: 22},
-    { name: 'Aron4',  idade: 23}
-  ];
+  // Video 67 ngFor
+  // name: string = 'Aron';
+  // minhaProp: boolean = false;
+  // personSelectedIndex: number | undefined;
+  
+  // listPeople = [
+  //   { name: 'Aron',   idade: 20},
+  //   { name: 'Aron 2', idade: 21},
+  //   { name: 'Aron3',  idade: 22},
+  //   { name: 'Aron4',  idade: 23}
+  // ];
 
-  handleInputChange(event: any)
-  {
-    console.log(event);
-    this.name = event;
-  }
+  // handleInputChange(event: any)
+  // {
+  //   console.log(event);
+  //   this.name = event;
+  // }
 
-  toggleProp()
-  {
-    this.minhaProp = !this.minhaProp;
-  }  
+  // toggleProp()
+  // {
+  //   this.minhaProp = !this.minhaProp;
+  // }  
 
-  selectedPerson(index: number)
-  {
-    this.personSelectedIndex = index
-  }
+  // selectedPerson(index: number)
+  // {
+  //   this.personSelectedIndex = index
+  // }
   
 
 }
